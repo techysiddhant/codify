677c902b4b38fefb0ead6402a5d4507c0faaa3ee
techysiddhant <sidjain8000@gmail.com>
2024-03-23 01:27:01 +0530

    added the logo text

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 
 export const Logo = () => {
-  return (
-    <Image
-      height={130}
-      width={130}
-      alt="logo"
-      src="/logo.svg"
-    />
-  )
-}
+	return (
+		// <Image
+		//   height={130}
+		//   width={130}
+		//   alt="logo"
+		//   src="/logo.svg"
+		// />
+		<h1 className="text-xl font-medium text-sky-700">Lots of Coding</h1>
+	);
+};
